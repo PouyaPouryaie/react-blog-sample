333c396aeb0a40425b69e4046b221df6b550d8e5
pouya <pouyapouryaie@gmail.com>
2024-04-02 20:40:31 -0300

    blog Sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect} from 'react';
+import { format } from 'date-fns';
 
 function App() {
+  const [posts, setPosts] = useState (
+    [
+      {
+        id: 1,
+        title: "My First Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+      },
+      {
+        id: 2,
+        title: "My Second Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+      }
+    ]
+  )
+
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+
+  /*
+    to serve component instead of requesting anyting from server,
+    we are serving the component that is routed to home directory by using browser history
+  */
+  const history = useHistory();
+
+  useEffect( () => {
+    const filteredResults = posts.filter(post => (
+        (post.body).toLowerCase()).includes(search.toLowerCase())
+        ||
+        ((post.title).toLowerCase()).includes(search.toLowerCase())
+      );
+
+      setSearchResult(filteredResults.reverse());
+  }, [posts, search]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [ ...posts, newPost ];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  }
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    history.push('/');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch}/>
+      <Switch>
+        {/* if we don't use exact for '/', all the other routes is similar with home endpoint and react dosen't route to other page
+            note: when your route is share between different endpoint, you need to use `exact` keyword */}
+        <Route exact path="/">
+          <Home posts={searchResult} />
+        </Route>
+        <Route exact path="/post">
+          <NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle} 
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody} 
+          />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage posts={posts} handleDelete={handleDelete} />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/PostPage.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useParams, Link } from 'react-router-dom';
+
+/*
+  useParams initiate to param that calls id, the reason is we define "id" for routing in app.js => (/posts/:id)
+*/
+
+const PostPage = ( {posts, handleDelete} ) => {
+
+  const {id} = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
+  return (
+    <main className='PostPage'>
+        <article className='post'>
+          {/* "post &&" means if post exist then render next part */}
+           {post && 
+            <>
+              <h2>{post.title}</h2>
+              <p className='postDate'>{post.datetime}</p>
+              <p className='postBody'>{post.body}</p>
+              <button onClick={ () => handleDelete(post.id)}>
+                Delete Post
+              </button>
+            </>
+           }
+           {!post &&
+            <>
+              <h2>Post Not Found</h2>
+              <p>Well, That's disappointing.</p>
+              <p>
+                <Link to="/">Visit Our HomePage</Link>
+              </p>
+            </> 
+           }
+        </article>
+    </main>
+  )
+}
+
+export default PostPage
